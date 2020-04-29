a62282b46dc06af0bc49165ea95dc5ceb6b8c972
scandarutice <60613141+scandarutice@users.noreply.github.com>
2020-04-29 11:59:10 +0900

    ユーザー登録機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,11 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content, :image).merge(user_id: current_user.id)
+  end
 end
